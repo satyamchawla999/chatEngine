d0eccd028a3784464e4be3074dcb9014cc97761d
Satyam Chawla <satyamchawla999@gmail.com>
2023-06-29 07:46:35 +0530

    handleClear

--- src/Components/subComponents/OpenChat.js
+++ src/Components/subComponents/OpenChat.js
@@ -3,163 +3,235 @@ import { ChatRoom } from "../subComponents";
 import { UseSelector, useSelector } from "react-redux/es/hooks/useSelector";
 
 import { db } from "../../firebase/firebase";
-import { getDocs, query, collection, updateDoc, where, onSnapshot } from "firebase/firestore";
-
+import {
+  getDocs,
+  query,
+  collection,
+  updateDoc,
+  where,
+  onSnapshot,
+} from "firebase/firestore";
 
 const OpenChat = (props) => {
-    const { user, currentUser } = props;
-    const userData = useSelector((state) => state.userData)
-    const name = user?.name;
+  const { user, currentUser } = props;
+  const userData = useSelector((state) => state.userData);
+  const name = user?.name;
 
-    const [message, setMessage] = useState("");
-    const [typing, setTyping] = useState(false);
+  const [message, setMessage] = useState("");
+  const [typing, setTyping] = useState(false);
 
-    const [chats, setChats] = useState([]);
+  const [chats, setChats] = useState([]);
 
+  let chatData = [];
+  useEffect(() => {
+    const getChats = async () => {
+      const chatId =
+        user.uid > userData.uid
+          ? userData.uid + user.uid
+          : user.uid + userData.uid;
+      const q = query(
+        collection(db, "ChatRoom"),
+        where("chatId", "==", chatId)
+      );
+      const unsubscribe = onSnapshot(q, (snapshot) => {
+        snapshot.forEach((doc) => {
+          const chats = doc.data().chats;
+          const cd = userData.uid;
 
-    let chatData = [];
-    useEffect(() => {
+          setTyping(doc.data()[cd]);
+          console.log(cd, doc.data()[cd]);
+          const updatedChats = chats.map((chat) => {
+            if (chat.reciverId === userData.uid) {
+              return {
+                ...chat,
+                status: true,
+              };
+            }
+            return chat;
+          });
 
-        const getChats = async () => {
-            const chatId = user.uid > userData.uid ? userData.uid + user.uid : user.uid + userData.uid;
-            const q = query(collection(db, "ChatRoom"), where("chatId", "==", chatId));
-            const unsubscribe = onSnapshot(q, (snapshot) => {
-                snapshot.forEach((doc) => {
-                    const chats = doc.data().chats;
-                    const cd = userData.uid;
+          const docRef = doc.ref;
+          updateDoc(docRef, { chats: updatedChats });
+        });
 
-                    setTyping(doc.data()[cd]);
-                    console.log(cd3, doc.data()[cd]);
-                    const updatedChats = chats.map((chat) => {
-                        if (chat.reciverId === userData.uid) {
-                            return {
-                                ...chat,
-                                status: true
-                            };
-                        }
-                        return chat;
-                    });
+        const updatedChats = snapshot.docs.flatMap((doc) => doc.data().chats);
+        setChats(updatedChats);
+      });
 
-                    const docRef = doc.ref;
-                    updateDoc(docRef, { chats: updatedChats });
-                });
+      return () => unsubscribe();
+    };
 
-                const updatedChats = snapshot.docs.flatMap((doc) => doc.data().chats);
-                setChats(updatedChats);
-            });
+    getChats();
+  }, [user, message]);
 
-            return () => unsubscribe();
-        };
+  console.log(chatData);
 
-        getChats();
-    }, [user, message]);
+  const handleMessage = (e) => {
+    e.preventDefault();
 
-    console.log(chatData);
+    if (message == "") {
+      alert("empty");
+      return;
+    }
 
-    const handleMessage = (e) => {
-        e.preventDefault();
+    const chatId =
+      user.uid > userData.uid
+        ? userData.uid + user.uid
+        : user.uid + userData.uid;
 
+    const date = new Date();
+    console.log(date);
 
-        if (message == "") {
-            alert("empty");
-            return;
+    const data = {
+      text: message,
+      status: false,
+      senderId: userData.uid,
+      reciverId: user.uid,
+      id: userData.uid,
+      chatId: chatId,
+      date: date,
+    };
+
+    const getChat = async () => {
+      const chatRoomRef = collection(db, "ChatRoom");
+      const querySnapshot = await getDocs(query(chatRoomRef));
+
+      querySnapshot.forEach((doc) => {
+        if (chatId === doc.data().chatId) {
+          const chatsArray = doc.data().chats || [];
+          chatsArray.push(data);
+
+          updateDoc(doc.ref, { chats: chatsArray });
         }
+      });
+    };
 
-        const chatId = user.uid > userData.uid ? userData.uid + user.uid : user.uid + userData.uid;
+    getChat();
 
-        const date = new Date();
-        console.log(date);
+    setMessage("");
+  };
+  let t;
+  const handleChange = async (e) => {
+    clearTimeout(t);
 
-        const data = {
-            text: message,
-            status: false,
-            senderId: userData.uid,
-            reciverId: user.uid,
-            id: userData.uid,
-            chatId: chatId,
-            date: date
-        };
+    setMessage(e.target.value);
+    const chatId =
+      user.uid > userData.uid
+        ? userData.uid + user.uid
+        : user.uid + userData.uid;
 
-        const getChat = async () => {
-            const chatRoomRef = collection(db, "ChatRoom");
-            const querySnapshot = await getDocs(query(chatRoomRef));
+    const q = query(collection(db, "ChatRoom"), where("chatId", "==", chatId));
+    const docs = await getDocs(q);
+    docs.forEach((doc) => {
+      const obj = {};
+      obj[user.uid] = true;
+      const docRef = doc.ref;
+      updateDoc(docRef, obj);
+    });
 
-            querySnapshot.forEach((doc) => {
-                if (chatId === doc.data().chatId) {
-                    const chatsArray = doc.data().chats || [];
-                    chatsArray.push(data);
+    t = setTimeout(() => {
+      docs.forEach((doc) => {
+        const obj = {};
+        obj[user.uid] = false;
+        const docRef = doc.ref;
+        updateDoc(docRef, obj);
+      });
+    }, 5000);
+  };
 
-                    updateDoc(doc.ref, { chats: chatsArray });
-                }
-            });
-        };
+  const handleClear = async ()=> {
+    const chatId =
+      user.uid > userData.uid
+        ? userData.uid + user.uid
+        : user.uid + userData.uid;
 
-        getChat();
+    const q = query(collection(db, "ChatRoom"), where("chatId", "==", chatId));
+    const docs = await getDocs(q);
+    docs.forEach((doc) => {
+      const arr = [];
+      const docRef = doc.ref;
+      updateDoc(docRef, {chats:arr});
+    });
+    setMessage("");
+  }
 
-        setMessage("");
-    }
-    let t;
-    const handleChange = async (e) => {
-        clearTimeout(t);
+  return (
+    <div className="openChat">
+      <div className="headBar">
+        <div className="senderInfo">
+          <div className="openChatImg">
+            <img
+              src="https://cdn.iconscout.com/icon/free/png-512/free-avatar-375-456327.png?f=avif&w=256"
+              alt="userImg"
+            />
+            {user.online === true ? (
+              <p
+                style={{ color: "green", backgroundColor: "green" }}
+                className="online"
+              >
+                &nbsp;.&nbsp;
+              </p>
+            ) : (
+              <p className="online">&nbsp;.&nbsp;</p>
+            )}
+            <h2>{name}</h2>
+          </div>
 
-        setMessage(e.target.value);
-        const chatId = user.uid > userData.uid ? userData.uid + user.uid : user.uid + userData.uid;
-
-        const q = query(collection(db, "ChatRoom"), where("chatId", "==", chatId));
-        const docs = await getDocs(q);
-        docs.forEach((doc) => {
-            const obj = {}
-            obj[user.uid] = true;
-            const docRef = doc.ref;
-            updateDoc(docRef, obj);
-        })
-
-        t = setTimeout(() => {
-            docs.forEach((doc) => {
-                const obj = {}
-                obj[user.uid] = false;
-                const docRef = doc.ref;
-                updateDoc(docRef, obj);
-            })
-        }, 5000);
-
-
-    }
-
-    return (
-        <div className="openChat">
-            <div className="headBar">
-                <div className="senderInfo">
-                    <div className="openChatImg">
-                        <img src="https://cdn.iconscout.com/icon/free/png-512/free-avatar-375-456327.png?f=avif&w=256" alt="userImg" />
-                        {user.online === true ? <p style={{ color: "green", backgroundColor: "green" }} className="online">&nbsp;.&nbsp;</p> : (<p className="online">&nbsp;.&nbsp;</p>)}
-                        <h2>{name}</h2>
-                    </div>
-
-                    <div className="headSpace">
-                        <p><i className="fa-solid fa-phone"></i></p>
-                        <p><i className="fa-solid fa-laptop-file"></i></p>
-                        <p><i className="fa-solid fa-ellipsis"></i></p>
-                    </div>
-                </div>
-                {typing === true && <p>Typing...</p>}
-
-            </div>
-
-            <ChatRoom chats={chats} userId={user.uid} userDataId={userData.uid} />
-
-            <div className="textBar">
-                <form style={{ display: "flex", width: "100%", alignItems: "center" }} onSubmit={handleMessage}>
-                    <i style={{ marginRight: "10px" }} className="fa-solid fa-paperclip"></i>
-                    <input type="text" placeholder="Type a message." value={message} onChange={handleChange} />
-                    <i style={{ marginRight: "10px", marginLeft: "20px" }} className="fa-regular fa-face-smile"></i>
-                    <button onSubmit={handleMessage} style={{ backgroundColor: "transparent", border: "none", marginRight: "10px" }}><p><img src={require("../../Assets/Images/send-message.png")} /></p></button>
-                </form>
-
-            </div>
+          <div className="headSpace">
+            <p>
+              <i className="fa-solid fa-phone"></i>
+            </p>
+            <p>
+              <i className="fa-solid fa-laptop-file"></i>
+            </p>
+            <p>
+              <i className="fa-solid fa-ellipsis"></i>
+            </p>
+          </div>
         </div>
-    );
+        <div className="additionalInfo">
+          {typing === true && <p>Typing...</p>}
+          <p onClick={handleClear}>Clear Conversation</p>
+        </div>
+      </div>
 
-}
+      <ChatRoom chats={chats} userId={user.uid} userDataId={userData.uid} />
+
+      <div className="textBar">
+        <form
+          style={{ display: "flex", width: "100%", alignItems: "center" }}
+          onSubmit={handleMessage}
+        >
+          <i
+            style={{ marginRight: "10px" }}
+            className="fa-solid fa-paperclip"
+          ></i>
+          <input
+            type="text"
+            placeholder="Type a message."
+            value={message}
+            onChange={handleChange}
+          />
+          <i
+            style={{ marginRight: "10px", marginLeft: "20px" }}
+            className="fa-regular fa-face-smile"
+          ></i>
+          <button
+            onSubmit={handleMessage}
+            style={{
+              backgroundColor: "transparent",
+              border: "none",
+              marginRight: "10px",
+            }}
+          >
+            <p>
+              <img src={require("../../Assets/Images/send-message.png")} />
+            </p>
+          </button>
+        </form>
+      </div>
+    </div>
+  );
+};
 
 export default OpenChat;
